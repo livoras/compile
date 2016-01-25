c7868824ee9c48331ea71046421a53915ab31ed4
Livoras <livoras@163.com>
2016-01-26 00:32:31 +0800

    fix tests

--- vt/test/codegen.js
+++ vt/test/codegen.js
@@ -3,19 +3,18 @@ var util = require('util')
 var fs = require('fs')
 var codegen = require('../src/codegen')
 
-// var str = fs.readFileSync('./fixtures/test1.html', 'utf-8')
-// var str2 = fs.readFileSync('./fixtures/test2.html', 'utf-8')
+var str = fs.readFileSync('./fixtures/test1.html', 'utf-8')
+var str2 = fs.readFileSync('./fixtures/test2.html', 'utf-8')
 var str3 = fs.readFileSync('./fixtures/test3.html', 'utf-8')
 
-var root = (new Parser(str3)).parse()
-
-// var root = new Parser(str2).parse()
-// console.log(util.inspect(root, false, null))
-
-var root = new Parser(str3).parse()
 
 describe('Test for walk through the AST', function () {
   it('walk walk walk', function () {
+    var root = (new Parser(str)).parse()
+    codegen(root)
+    var root = (new Parser(str2)).parse()
+    codegen(root)
+    var root = (new Parser(str3)).parse()
     codegen(root)
   })
 })
